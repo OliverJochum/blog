34d231e842205997a867b02bc7eddd15602d9319
Oliver Jochum <oliver.jochum@yahoo.com>
2023-04-16 20:48:56 +0200

    removed redundancy

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,12 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments, dependent: :destroy
 
-  VALID_STATUSES = ['public','private','archived']
+  
 
   validates :title, presence: true
   validates :body, presence: true, length: {minimum: 10}
 
-  validates :status, inclusion: {in: VALID_STATUSES}
-
-  def archived?
-    status == 'archived'
-  end
+  
 end
